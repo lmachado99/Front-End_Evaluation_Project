537e838112a1f97d4d4f0a15e9600e77f3d88875
Lucas Greve <lucasag@ciandt.com>
2021-09-15 17:58:10 -0300

    Implementation of some components and alteration of the icon and title page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './modules/main-page/main-page.component';
+import { SignUpComponent } from './modules/sign-up/sign-up.component';
+import { SignUpSuccessComponent } from './modules/sign-up/sign-up-success/sign-up-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    SignUpComponent,
+    SignUpSuccessComponent
   ],
   imports: [
     BrowserModule,
