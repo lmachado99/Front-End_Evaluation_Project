8a13d8babf32f9b6b752da84b29736941ffc67ae
Lucas Greve <lucasag@ciandt.com>
2021-09-15 18:33:25 -0300

    Implementation of header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './modules/main-page/main-page.component';
 import { SignUpComponent } from './modules/sign-up/sign-up.component';
 import { SignUpSuccessComponent } from './modules/sign-up/sign-up-success/sign-up-success.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     SignUpComponent,
-    SignUpSuccessComponent
+    SignUpSuccessComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
