ff2c300822756bc5ac7d50a3479b9e68e0bfe398
Lucas Greve <lucasag@ciandt.com>
2021-09-16 19:32:49 -0300

    Benefits items implementation

--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Images } from 'src/assets/images/indice.enum';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  public devicePath: string;
+  public correctIconPath: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.devicePath = Images.device;
+    this.correctIconPath = Images.correctIcon;
   }
 
 }
--- src/assets/images/indice.enum.ts
+++ src/assets/images/indice.enum.ts
@@ -1,4 +1,6 @@
 export enum Images {
     logo = 'assets/images/logo.png',
     headerImage = 'assets/images/img_header.png',
+    device = 'assets/images/device.png',
+    correctIcon = 'assets/images/correct_icon.png'
 }
