cb34838ee4b9878c9c849683224879d11dfc60bb
Lucas Greve <lucasag@ciandt.com>
2021-09-17 19:41:08 -0300

    Footer component implementation

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Images } from 'src/assets/images/indice.enum';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public logoFooterPath: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.logoFooterPath = Images.logoFooter;
   }
 
 }
--- src/assets/images/indice.enum.ts
+++ src/assets/images/indice.enum.ts
@@ -1,8 +1,9 @@
 export enum Images {
-    logo = 'assets/images/logo.png',
+    logoHeader = 'assets/images/logo_header.png',
     headerImage = 'assets/images/img_header.png',
     device = 'assets/images/device.png',
     correctIcon = 'assets/images/correct_icon.png',
     linkIcon = 'assets/images/link_icon.png',
     phoneIcon = 'assets/images/phone_icon.png',
+    logoFooter = 'assets/images/logo_footer.png',
 }
