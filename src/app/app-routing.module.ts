74eea45e012ce8e7112e6a37b86ed10d4a20f48a
Lucas Greve <lucasag@ciandt.com>
2021-09-18 09:09:11 -0300

    Sign-up page implementation and some maintenances on the button color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IntroductionComponent } from './modules/introduction/introduction.component';
+import { SignUpSuccessComponent } from './modules/sign-up/sign-up-success/sign-up-success.component';
 import { SignUpComponent } from './modules/sign-up/sign-up.component';
 
 
 const routes: Routes = [
   { path: '', component: IntroductionComponent },
-  { path: 'signup', component: SignUpComponent }
+  { path: 'signup', component: SignUpComponent },
+  { path: 'signup-success', component: SignUpSuccessComponent }
 ];
 
 @NgModule({
